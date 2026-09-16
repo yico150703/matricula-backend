13c32cb222c1ffd44e3948641a0f95e9e880eb7a
Anderson Murrugarra <yico150703@gmail.com>
2026-09-15 21:37:47 -0500

    feat: allow querying all sections for a period

--- app/blueprints/secciones.py
+++ app/blueprints/secciones.py
@@ -9,8 +9,9 @@ bp = Blueprint("secciones", __name__)
 def available_sections(id_periodo):
     PeriodoAcademico.query.get_or_404(id_periodo)
     id_curso = request.args.get("curso", type=int)
-    if not id_curso:
-        raise ApiError("curso_requerido", "El parámetro de consulta curso es obligatorio.", 400)
-    Curso.query.get_or_404(id_curso)
-    sections = Seccion.query.filter_by(id_periodo=id_periodo, id_curso=id_curso).order_by(Seccion.nro_seccion).all()
+    query = Seccion.query.filter_by(id_periodo=id_periodo)
+    if id_curso:
+        Curso.query.get_or_404(id_curso)
+        query = query.filter_by(id_curso=id_curso)
+    sections = query.order_by(Seccion.id_curso, Seccion.nro_seccion).all()
     return jsonify(secciones=[section.to_dict() for section in sections])
